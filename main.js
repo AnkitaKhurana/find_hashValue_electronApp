11d012f7ea47328f45b067db0b99e8ab8b5bc598
Ankita Khurana <ankita0096@gmail.com>
2018-02-09 15:45:38 +0530

    main.js

---
+++ main.js
@@ -0,0 +1,43 @@
+const {app , BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+
+let win = null;
+//global reference to window object 
+
+
+
+function createWindow()
+{
+	win = new BrowserWindow({windth:800,height:600,backgroundColor: "#D6D8DC"});
+
+	win.loadURL(url.format({
+		pathname : path.join(__dirname , 'index.html'),
+		protocol : 'file:',
+		slashes : true
+	}));
+
+	//win.webContents.openDevTools()
+	
+	win.on('closed',()=>{
+		win = null;
+	});
+}
+
+
+
+app.on('ready',createWindow);
+app.on('window-all-closed',()=>{
+	if(process.platform!='darwin')
+	{
+		app.quit();
+	}
+});
+
+app.on('activate',()=>{
+	if(win==null){
+		createWindow();
+	}
+
+});
